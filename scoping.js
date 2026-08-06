3ffc731a90034e5aa6988060cd3eda31f11a1c4c
Ashwini2680 <mail.ashwini26@gmail.com>
2026-08-06 09:45:08 +0530

    DataType_and_Scoping_HandsOn
    Homework on DataType and Scopiing changes.

---
+++ scoping.js
@@ -0,0 +1,13 @@
+const { log } = require("node:console");
+
+const browserversion = "chrome";
+
+function  getBrowserVersion () {
+    if(browserversion == "chrome"){
+        let browserversion = "chrome";
+    }
+    console.log(browserversion);
+    
+}
+getBrowserVersion ()
+ 
